dedfa53bec9e7f8724578c062ff27dc5e71d06fc
Roman Makulin <sup.makulin@mail.ru>
2025-01-25 21:13:15 +0300

    Фикс конфига безопасности для корректной обработки post запросов в том числе (отключи csrf) + изменил эндпоинт

--- parent-project/auth/src/main/java/com/example/auth/config/SecurityConfig.java
+++ parent-project/auth/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -41,12 +42,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                // Отключение CSRF, так как мы используем JWT
+                .csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults()) // проверка JWT токена с учетом issuer-uri
+                        .jwt(Customizer.withDefaults()) // Проверка JWT токена с учетом issuer-uri
                 );
 
         return http.build();
