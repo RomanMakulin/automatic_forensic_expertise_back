0847d883bf3e458906576b928f835b2cbb836e2b
Roman Makulin <sup.makulin@mail.ru>
2025-01-27 12:49:37 +0300

    Создал модуль миграций БД для приложения

--- parent-project/test/src/main/java/com/example/test/SecurityConfig.java
+++ parent-project/test/src/main/java/com/example/test/SecurityConfig.java
@@ -5,12 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.web.SecurityFilterChain;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
@@ -18,15 +12,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // 1) Все запросы к API по умолчанию требуют аутентификации
-        // 2) Некоторые открытые (публичные) эндпоинты можно разрешить без токена
-        // 3) Включаем поддержку проверки JWT через oauth2ResourceServer().jwt()
-
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/public/**").permitAll()
-                        // Разрешить, например, доступ к swagger, actuator, health:
-                        // .requestMatchers("/swagger-ui/**","/v3/api-docs/**","/actuator/health").permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
