e4bef03777705d7f34ac7723680b14868be1a7d0
Roman Makulin <sup.makulin@mail.ru>
2025-01-31 19:23:24 +0300

    Восстановил admin service

---
+++ parent-project/admin-service/src/main/java/com/example/adminservice/config/CorsConfigProperties.java
@@ -0,0 +1,29 @@
+package com.example.adminservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Настройки конфигурации CORS
+ */
+@Component
+@ConfigurationProperties(prefix = "cors")
+public class CorsConfigProperties {
+
+    /**
+     * Разрешенные домены.
+     */
+    private List<String> allowedOrigins;
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
---
+++ parent-project/admin-service/src/main/java/com/example/adminservice/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.adminservice.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Конфигурация безопасности модуля
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * Конфигурация безопасности
+     *
+     * @param http объект для конфигурации безопасности
+     * @return объект для конфигурации безопасности
+     * @throws Exception ошибка
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()) // проверка JWT токена с учетом issuer-uri
+                );
+
+        return http.build();
+    }
+
+}
