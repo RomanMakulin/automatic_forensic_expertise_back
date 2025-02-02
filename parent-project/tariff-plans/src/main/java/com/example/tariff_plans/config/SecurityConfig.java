795c6a05e3feb7daee69665ff3926b53c869cd2c
karac <freelook22@mail.ru>
2025-02-03 01:12:56 +0300

    тарифы с безопастностью романа поднимаются. применил ломбок у никиты

---
+++ parent-project/tariff-plans/src/main/java/com/example/tariff_plans/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.tariff_plans.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Конфигурация безопасности приложения.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * Конфигурация безопасности
+     *
+     * @param http объект для конфигурации безопасности
+     * @return объект для конфигурации безопасности
+     * @throws Exception ошибка
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Отключение CSRF, так как мы используем JWT
+                .cors(Customizer.withDefaults()) // включаем CORS
+                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()) // Проверка JWT токена с учетом issuer-uri
+                );
+
+        return http.build();
+    }
+
+}
