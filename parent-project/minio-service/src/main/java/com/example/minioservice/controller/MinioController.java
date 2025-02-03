543008005e52bfaef8b47d33607a7ca8053be7d9
Roman Makulin <sup.makulin@mail.ru>
2025-02-03 14:50:17 +0300

    Удалил лишний импорт

--- parent-project/minio-service/src/main/java/com/example/minioservice/controller/MinioController.java
+++ parent-project/minio-service/src/main/java/com/example/minioservice/controller/MinioController.java
@@ -5,10 +5,12 @@ import com.example.minioservice.dto.FileDto;
 import com.example.minioservice.service.MinioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.swing.*;
 import java.util.List;
 import java.util.UUID;
 
